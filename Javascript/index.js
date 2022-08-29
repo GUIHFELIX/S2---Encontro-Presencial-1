e63ecfe7289be8a98bf72e2fa441f95cc109a4d6
Aluno <guifelix339@gmail.com>
2022-08-29 15:11:47 -0300

    correção atv1

---
+++ Javascript/index.js
@@ -0,0 +1,15 @@
+function fonte(e){
+	let elemento = $(".acessibilidade");
+	let fonte = elemento.css('font-size');
+
+	if (e == 'a') {
+		elemento.css("fontSize", parseInt(fonte) + 1);
+	} else if('d'){
+		elemento.css("fontSize", parseInt(fonte) - 1);
+	}
+}
+
+    function darkMode() {
+        var element = document.body;
+     element.classList.toggle("dark-mode");
+  }
